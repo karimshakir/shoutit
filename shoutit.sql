f0b80ad3c21175a52b8d21ed1c0a953aea2a4012
karimshakir <karimshakir99@gmail.com>
2018-02-22 20:17:47 -0600

    adds shoutit.sql back

---
+++ shoutit.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 21, 2018 at 06:09 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoutit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shouts`
+--
+
+CREATE TABLE `shouts` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shouts`
+--
+
+INSERT INTO `shouts` (`id`, `user`, `message`, `time`) VALUES
+(1, 'Paul', 'This is what then Paul yesterday and, a tendency to try to create.', '11:20:00'),
+(3, 'Jen', 'Versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', '11:50:00'),
+(4, 'Brad', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.', '11:23:00'),
+(5, 'John', 'Letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their.', '11:24:00'),
+(6, 'Sam', 'Publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', '11:30:00'),
+(9, 'Reem', 'Chekcing', '00:00:02'),
+(10, 'Karim', 'Checking ', '00:00:02'),
+(11, 'john', 'whatever', '00:00:06'),
+(12, 'Jeff', 'What\'s up tonight?', '00:00:10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shouts`
+--
+ALTER TABLE `shouts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shouts`
+--
+ALTER TABLE `shouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
